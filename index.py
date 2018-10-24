0a297c7271fac80ae20659af0b1e3d796f04223e
hkussie <hkussie@gmail.com>
2018-10-23 17:11:32 -0700

    First Commit

--- index.py
+++ index.py
@@ -4,11 +4,13 @@ import time
 
 browser = webdriver.Firefox()
 browser.get('https://www.indeed.com')
+
 assert "Job Search | Indeed" in browser.title
 elem = browser.find_element_by_name("q")
 elem.clear()
 elem.send_keys("Software Engineer")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in browser.page_source
+
 time.sleep(5)  
 browser.close()
