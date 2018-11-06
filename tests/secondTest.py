2537c7cee4621b7067389b7e5a85fe39e82f01d7
hkussie <hkussie@gmail.com>
2018-11-05 16:23:54 -0800

    Add changes to geckodriverlog

--- tests/secondTest.py
+++ tests/secondTest.py
@@ -3,41 +3,36 @@ import unittest
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 
-class testJobsPage(unittest.TestCase):
-	def createSelf(self):
-		self.driver = webdriver.Firefox()
-
-	def test_job_page(self):
-		driver = self.driver
-		element_header = get_elem('')
-
-	def tear_down(self):
-		time.sleep(10)
-		self.driver.close()
-
-if __name__ == "__main__":
-	unittest.main()	
-
-
-'''
-class testBeta(unittest.TestCase):
+class BasicSecurityTest(unittest.TestCase):
+	
 	def setUp(self):
 		self.driver = webdriver.Firefox()
 
-	def test_search_indeed(self):
+	def clearCookies(self):
 		driver = self.driver
-		driver.get("https://www.indeed.com")
-		self.assertIn("Indeed", driver.title)
-		elem = driver.find_element_by_name("q")
-		elem.send_keys("Software Engineer")
-		elem.send_keys(Keys.RETURN)
-		assert "No results found." not in driver.page_source
+		driver.get("https://Indeed.com") 
+		# driver.get_cookies()
 
 	def tearDown(self):
 		time.sleep(5)
 		self.driver.close()
 
 if __name__ == "__main__":
-	unittest.main()
-'''
+	unittest.main()				
 
+'''
+class testJobsPage(unittest.TestCase):
+	def setUp(self):
+		self.driver = webdriver.Firefox()
+
+	def test_main_page(self):
+		driver = self.driver
+		driver.get('https://www.indeed.com')
+
+	def tear_down(self):
+		time.sleep(3)
+		self.driver.close()
+
+if __name__ == "__main__":
+	unittest.main()	
+'''
